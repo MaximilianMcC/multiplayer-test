34bf89ae365af63398418562cd4eabed2a90e486
Max <max@maximilian.co.nz>
2023-09-24 19:01:51 +1300

    Setup server

---
+++ server/Program.cs
@@ -0,0 +1,12 @@
+﻿class Server
+{
+	public static void Main(string[] args)
+	{
+		// Title crap
+		Console.Title = "UDP Server";
+		Console.WriteLine("C# Multiplayer UDP server test");
+
+		// Parse the port from the args
+		int port = int.Parse(args[0].Trim());
+	}
+}
