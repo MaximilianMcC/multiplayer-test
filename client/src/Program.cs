03486cea6272c565ee158c524288d4881f467652
Max <max@maximilian.co.nz>
2024-01-26 23:52:01 +1300

    Args can be passed in to connect to server

--- client/src/Program.cs
+++ client/src/Program.cs
@@ -1,7 +1,18 @@
 ﻿class Program
 {
+	public static string[] Args;
+
 	public static void Main(string[] args)
 	{
+		// Set the args
+		Args = args;
+		if (Args.Length == 0)
+		{
+			// Show an error message if wrong args were given
+			Console.WriteLine("Insufficient arguments provided🤬\nYou are missing the server port, and ip. Provide them in a single string, separated with a colon. <IP>:<PORT>\nExample: client.exe 127.0.0.1:12345");
+			return;
+		}
+
 		Game.Run();
 	}
 }
